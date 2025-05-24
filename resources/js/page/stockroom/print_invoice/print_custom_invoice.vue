47ca4d1a8be56aa118c3bdc44bda07a690ac267c
Jundell <jundell.ggare@gmail.com>
2025-05-24 08:40:59 +0800

    h

--- resources/js/page/stockroom/print_invoice/print_custom_invoice.vue
+++ resources/js/page/stockroom/print_invoice/print_custom_invoice.vue
@@ -4,7 +4,6 @@
       <!-- Left Panel: Form Inputs -->
       <div class="col-md-4" id="leftPanel">
         <h3>Order Information</h3>
-
         <div v-for="field in formFields" :key="field.id" class="mb-3">
           <div class="d-flex align-items-center mb-1">
             <label :for="field.id" class="form-label me-2" style="min-width: 150px;">{{ field.label }}</label>
@@ -43,40 +42,86 @@
         <div class="d-flex justify-content-between">
           <button class="btn btn-outline-primary" @click="downloadAsPDF">Export as PDF</button>
           <button class="btn btn-success" @click="submitInvoice">Submit Invoice</button>
+          <button class="btn btn-dark" @click="sendToZebraPrinter">Send to Printer</button>
         </div>
 
-        <div id="designPreview" class="mt-4 p-3 border">
-          <h4>Ship To:</h4>
+        <div id="designPreview" class="mt-4 p-3 border bg-white">
+          <h4><strong>Ship To:</strong></h4>
           <p><strong>{{ form.CustomerName }}</strong></p>
           <p>{{ form.AddressLine1 }}</p>
           <p>{{ form.City }}, {{ form.StateOrRegion }}, {{ form.PostalCode }}, {{ form.CountryCode }}</p>
 
+          <p><strong>Order ID:</strong> {{ form.AmazonOrderId }}</p>
           <svg id="barcode_AmazonOrderId"></svg>
-          <p>Order ID: {{ form.AmazonOrderId }}</p>
 
-          <div class="mt-3">
-            <h5>Items</h5>
+          <p class="mt-2">Thank you for buying from ... on Amazon Marketplace.</p>
+
+          <table class="table table-sm table-borderless mt-3">
+            <tbody>
+              <tr>
+                <td class="max-width-cell"><strong>Shipping Address:</strong></td>
+                <td class="max-width-cell"><strong>Order Date:</strong></td>
+                <td class="max-width-cell">{{ form.OrderDate }}</td>
+              </tr>
+              <tr>
+                <td class="max-width-cell">{{ form.CustomerName }}</td>
+                <td class="max-width-cell"><strong>Ship by Date:</strong></td>
+                <td class="max-width-cell">{{ form.ShipByDateEarliest }} - {{ form.ShipByDateLatest }}</td>
+              </tr>
+              <tr>
+                <td class="max-width-cell">{{ form.AddressLine1 }}</td>
+                <td class="max-width-cell"><strong>Deliver by Date:</strong></td>
+                <td class="max-width-cell">{{ form.DeliveryByDateEarliest }} - {{ form.DeliveryByDateLatest }}</td>
+              </tr>
+              <tr>
+                <td class="max-width-cell">{{ form.City }}, {{ form.StateOrRegion }}</td>
+                <td class="max-width-cell"><strong>Shipping Service:</strong></td>
+                <td class="max-width-cell">{{ form.ShippingService }}</td>
+              </tr>
+              <tr>
+                <td class="max-width-cell">{{ form.CountryCode }}, {{ form.PostalCode }}</td>
+                <td class="max-width-cell"><strong>Buyer Name:</strong></td>
+                <td class="max-width-cell">{{ form.CustomerName.split(' ')[0] }}</td>
+              </tr>
+              <tr>
+                <td class="max-width-cell"></td>
+                <td class="max-width-cell"><strong>Seller Name:</strong></td>
+                <td class="max-width-cell">{{ form.SellerName }}</td>
+              </tr>
+            </tbody>
+          </table>
+
+          <div v-for="(item, index) in items" :key="index" class="mt-4">
             <table class="table table-bordered">
               <thead>
                 <tr>
-                  <th>Title</th>
-                  <th>ASIN</th>
-                  <th>MSKU</th>
-                  <th>Qty</th>
+                  <th style="width: 20%;">Quantity</th>
+                  <th>Product Details</th>
                 </tr>
               </thead>
               <tbody>
-                <tr v-for="(item, index) in items" :key="index">
-                  <td>{{ item.Title }}</td>
-                  <td>{{ item.ASIN }}</td>
-                  <td>{{ item.MSKU }}</td>
+                <tr>
                   <td>{{ item.Quantity }}</td>
+                  <td>
+                    <strong>{{ item.Title }}</strong><br />
+                    <strong>SKU:</strong> {{ item.MSKU }}<br />
+                    <strong>ASIN:</strong> {{ item.ASIN }}<br />
+                    <svg :id="`barcode_ASIN_${index}`"></svg><br />
+                    <strong>Condition:</strong> {{ item.Condition || 'New - New' }}<br />
+                    <strong>Order Item ID:</strong> {{ item.OrderItemId || 'N/A' }}<br />
+                    <strong>P Code:</strong> $ {{ item.PCode || 'X.XX' }}<br />
+                    <strong>S Code:</strong> $ {{ item.SCode || 'X.XX' }}<br />
+                    <strong>L Code:</strong> $ {{ item.LCode || 'X.XX' }}
+                  </td>
+                </tr>
+                <tr>
+                  <td colspan="2"><strong>Note:</strong> {{ item.Note || '' }}</td>
                 </tr>
               </tbody>
             </table>
           </div>
 
-          <div class="mt-3 text-end">
+          <div class="text-end mt-3">
             <p><strong>Created by:</strong> {{ form.labelcreator }}</p>
           </div>
         </div>
@@ -152,6 +197,7 @@ export default {
       ]
     };
   },
+
   watch: {
     'form.AmazonOrderId': {
       handler(newVal) {
@@ -162,7 +208,7 @@ export default {
   },
   methods: {
     toggleFieldVisibility(id) {
-      this.$set(this.fieldVisibility, id, !this.fieldVisibility[id]);
+      this.fieldVisibility[id] = !this.fieldVisibility[id];
     },
     addItem() {
       this.items.push({ Title: "", ASIN: "", MSKU: "", Quantity: 1 });
@@ -190,6 +236,57 @@ export default {
     submitInvoice() {
       alert("Invoice submitted (mock).");
       // Submit logic goes here
+    },
+    generateZPL() {
+      const { form, items } = this;
+
+      let zpl = "^XA";
+      zpl += `^FO50,50^ADN,36,20^FDShip To:^FS`;
+      zpl += `^FO50,90^FD${form.CustomerName}^FS`;
+      zpl += `^FO50,130^FD${form.AddressLine1}^FS`;
+      zpl += `^FO50,170^FD${form.City}, ${form.StateOrRegion}^FS`;
+      zpl += `^FO50,210^FD${form.PostalCode}, ${form.CountryCode}^FS`;
+
+      zpl += `^FO50,260^FDOrder ID: ${form.AmazonOrderId}^FS`;
+      zpl += `^FO50,300^FDOrder Date: ${form.OrderDate}^FS`;
+      zpl += `^FO50,340^FDShip by: ${form.ShipByDateEarliest} - ${form.ShipByDateLatest}^FS`;
+      zpl += `^FO50,380^FDDeliver by: ${form.DeliveryByDateEarliest} - ${form.DeliveryByDateLatest}^FS`;
+      zpl += `^FO50,420^FDShipping Service: ${form.ShippingService}^FS`;
+
+      let y = 470;
+      items.forEach((item, index) => {
+        zpl += `^FO50,${y}^FDQty: ${item.Quantity} - ${item.Title}^FS`;
+        y += 40;
+        zpl += `^FO50,${y}^FDSKU: ${item.MSKU} ASIN: ${item.ASIN}^FS`;
+        y += 40;
+        zpl += `^FO50,${y}^FDCondition: ${item.Condition || 'New - New'}^FS`;
+        y += 40;
+        zpl += `^FO50,${y}^FDOrderItemId: ${item.OrderItemId || 'N/A'}^FS`;
+        y += 40;
+        zpl += `^FO50,${y}^FDNote: ${item.Note || ''}^FS`;
+        y += 60;
+      });
+
+      zpl += `^FO50,${y}^FDCreated by: ${form.labelcreator}^FS`;
+      zpl += "^XZ";
+
+      return zpl;
+    },
+
+    async sendToZebraPrinter() {
+      const zplFullCommand = this.generateZPL();
+      const pIp = this.form.printerIp || "192.168.1.240";
+
+      try {
+        const res = await axios.post("http://99.0.87.190:1450/ims/Admin/modules/PRD-RPN-PCN/print.php", {
+          zpl: zplFullCommand,
+          printerSelect: pIp,
+        });
+        alert("Sent to printer successfully");
+      } catch (err) {
+        console.error("Printer error", err);
+        alert("Failed to send to printer");
+      }
     }
   }
 };
@@ -199,5 +296,11 @@ export default {
 #designPreview {
   background: #fff;
   font-size: 14px;
+}
+
+.max-width-cell {
+  max-width: 200px;
+  word-break: break-word;
+  white-space: normal;
 }
 </style>
