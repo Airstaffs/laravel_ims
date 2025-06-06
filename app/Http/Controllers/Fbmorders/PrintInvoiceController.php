5f9c55dfa4a8b8452abb6b6c0d5c6cee3f547fd5
Jundell <jundell.ggare@gmail.com>
2025-06-07 05:48:34 +0800

    rawr

--- app/Http/Controllers/Fbmorders/PrintInvoiceController.php
+++ app/Http/Controllers/Fbmorders/PrintInvoiceController.php
@@ -1,1 +1,24 @@
 <?php
+
+namespace App\Http\Controllers\Fbmorders;
+
+use Mpdf\Mpdf;
+use Imagick;
+use ImagickPixel;
+use Illuminate\Support\Facades\Storage;
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use Carbon\Carbon;
+use PhpOffice\PhpSpreadsheet\Spreadsheet;
+use PhpOffice\PhpSpreadsheet\Writer\Xlsx;
+use PhpOffice\PhpSpreadsheet\Style\Alignment;
+use PhpOffice\PhpSpreadsheet\Style\Border;
+use PhpOffice\PhpSpreadsheet\Style\Fill;
+use Illuminate\Support\Facades\Log;
+
+class PrintInvoiceController extends Controller {
+    public function PrintInvoice(Request $request) {
+        
+    }
+}
