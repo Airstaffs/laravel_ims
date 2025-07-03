e52eae8e65828f50b10930d92309cea60e5ffe0c
danairstaffs <dan@airstaffs.com>
2025-07-04 07:43:12 +0800

    Add FNSKU Modal

--- resources/js/page/fnsku/fnsku.vue
+++ resources/js/page/fnsku/fnsku.vue
@@ -22,24 +22,6 @@
                                 v-model="selectAll"
                             />
                         </th>
-                        <th class="sticky-header second-sticky">
-                            <div class="product-name">
-                                <span
-                                    class="sortable"
-                                    @click="sortBy('AStitle')"
-                                >
-                                    Product Name
-                                    <i
-                                        v-if="sortColumn === 'AStitle'"
-                                        :class="
-                                            sortOrder === 'asc'
-                                                ? 'fas fa-sort-up'
-                                                : 'fas fa-sort-down'
-                                        "
-                                    ></i>
-                                </span>
-                            </div>
-                        </th>
                         <th class="">ASIN</th>
                         <th class="">FNSKU</th>
                         <th class="">MSKU</th>
@@ -54,22 +36,6 @@
                     <tr v-for="(item, index) in inventory" :key="item.FNSKUID">
                         <td class="sticky-col first-col">
                             <input type="checkbox" v-model="item.checked" />
-                        </td>
-                        <td class="sticky-col second-sticky">
-                            <div class="product-container">
-                                <div
-                                    class="product-image-container"
-                                    @click="openImageModal(item)"
-                                >
-                                    <!-- Use the actual file path for the main image -->
-                                    <img
-                                        :src="'/images/thumbnails/' + item.img1"
-                                        :alt="item.ProductTitle || 'Product'"
-                                        class="product-thumbnail clickable-image"
-                                        @error="handleImageError($event)"
-                                    />
-                                </div>
-                            </div>
                         </td>
                         <td>
                             <span>
@@ -160,34 +126,9 @@
                     v-for="(item, index) in inventory"
                     :key="item.FNSKUID"
                 >
-                    <div class="mobile-card-header">
+                    <div class="mobile-card-details">
                         <div class="mobile-checkbox">
                             <input type="checkbox" v-model="item.checked" />
-                        </div>
-                        <div class="mobile-product-image clickable">
-                            <img
-                                :src="'/images/thumbnails/' + item.img1"
-                                :alt="item.ProductTitle || 'Product'"
-                                class="product-thumbnail"
-                                @click="openImageModal(item)"
-                                @error="handleImageError($event)"
-                            />
-                        </div>
-                        <div class="mobile-product-info">
-                            <h3 class="mobile-product-name clickable">
-                                {{ item.astitle }}
-                            </h3>
-                        </div>
-                    </div>
-
-                    <hr />
-
-                    <div class="mobile-card-details">
-                        <div class="mobile-detail-row">
-                            <span class="mobile-detail-label">ID:</span>
-                            <span class="mobile-detail-value">{{
-                                item.FNSKUID
-                            }}</span>
                         </div>
                         <div class="mobile-detail-row">
                             <span class="mobile-detail-label">ASIN:</span>
@@ -208,7 +149,7 @@
                             }}</span>
                         </div>
                         <div class="mobile-detail-row">
-                            <span class="mobile-detail-label">Condition:</span>
+                            <span class="mobile-detail-label">Grading:</span>
                             <span
                                 class="mobile-detail-value badge text-white"
                                 :class="{
@@ -240,7 +181,7 @@
                             </span>
                         </div>
                         <div class="mobile-detail-row">
-                            <span class="mobile-detail-label">Store:</span>
+                            <span class="mobile-detail-label">Store name:</span>
                             <span class="mobile-detail-value">{{
                                 item.storename
                             }}</span>
@@ -249,12 +190,6 @@
                             <span class="mobile-detail-label">Units:</span>
                             <span class="mobile-detail-value">{{
                                 item.Units
-                            }}</span>
-                        </div>
-                        <div class="mobile-detail-row">
-                            <span class="mobile-detail-label">Total Qty:</span>
-                            <span class="mobile-detail-value">{{
-                                item.totalquantity
                             }}</span>
                         </div>
                     </div>
@@ -333,7 +268,9 @@
         </div>
 
         <!-- Insert FNSKU Modal -->
-        <div class="fnsku modal" v-if="isInsertFnskuModalVisible">
+        <div class="modal fnsku-modal" v-if="isInsertFnskuModalVisible">
+            <div class="modal-overlay" @click="hideInsertFnskuModal"></div>
+
             <div class="modal-content">
                 <div class="modal-header">
                     <h2>Add New FNSKU</h2>
@@ -341,99 +278,94 @@
                         >&times;</span
                     >
                 </div>
+
                 <div class="modal-body">
-                    <div class="form-group">
-                        <label for="newFnsku">FNSKU:</label>
-                        <input
-                            type="text"
-                            id="newFnsku"
-                            v-model="newFnskuData.fnsku"
-                            placeholder="Enter FNSKU"
-                            class="form-control"
-                            ref="newFnskuInput"
-                            @input="focusNext('newMskuInput')"
-                        />
-                    </div>
+                    <form class="fnskuForm">
+                        <fieldset>
+                            <label><span>FNSKU:</span></label>
+                            <input
+                                type="text"
+                                id="newFnsku"
+                                v-model="newFnskuData.fnsku"
+                                placeholder="Enter FNSKU"
+                                class="form-control"
+                                ref="newFnskuInput"
+                                @input="focusNext('newMskuInput')"
+                            />
+                        </fieldset>
 
-                    <div class="form-group">
-                        <label for="newMsku">MSKU:</label>
-                        <input
-                            type="text"
-                            id="newMsku"
-                            v-model="newFnskuData.msku"
-                            placeholder="Enter MSKU"
-                            class="form-control"
-                            ref="newMskuInput"
-                            @input="focusNext('newAsinInput')"
-                        />
-                    </div>
+                        <fieldset>
+                            <label><span>MSKU:</span></label>
+                            <input
+                                type="text"
+                                id="newMsku"
+                                v-model="newFnskuData.msku"
+                                placeholder="Enter MSKU"
+                                class="form-control"
+                                ref="newMskuInput"
+                                @input="focusNext('newAsinInput')"
+                            />
+                        </fieldset>
 
-                    <div class="form-group">
-                        <label for="newAsin">ASIN:</label>
-                        <input
-                            type="text"
-                            id="newAsin"
-                            v-model="newFnskuData.asin"
-                            placeholder="Enter ASIN"
-                            class="form-control"
-                            ref="newAsinInput"
-                            @input="focusNext('newTitleInput')"
-                        />
-                    </div>
+                        <fieldset>
+                            <label><span>ASIN:</span></label>
+                            <input
+                                type="text"
+                                id="newAsin"
+                                v-model="newFnskuData.asin"
+                                placeholder="Enter ASIN"
+                                class="form-control"
+                                ref="newAsinInput"
+                                @input="focusNext('newTitleInput')"
+                            />
+                        </fieldset>
 
-                    <div class="form-group">
-                        <label for="newTitle">Title:</label>
-                        <input
-                            type="text"
-                            id="newTitle"
-                            v-model="newFnskuData.astitle"
-                            placeholder="Enter Product Title"
-                            class="form-control"
-                            ref="newTitleInput"
-                            @input="focusNext('newGradingInput')"
-                        />
-                    </div>
+                        <fieldset>
+                            <label><span>Title:</span></label>
+                            <input
+                                type="text"
+                                id="newTitle"
+                                v-model="newFnskuData.astitle"
+                                placeholder="Enter Product Title"
+                                class="form-control"
+                                ref="newTitleInput"
+                                @input="focusNext('newGradingInput')"
+                            />
+                        </fieldset>
 
-                    <div class="form-group">
-                        <label for="newGrading">Grading:</label>
-                        <select
-                            id="newGrading"
-                            v-model="newFnskuData.grading"
-                            class="form-control"
-                            ref="newGradingInput"
-                        >
-                            <option value="New">New</option>
-                            <option value="Like New">Like New</option>
-                            <option value="Very Good">Very Good</option>
-                            <option value="Good">Good</option>
-                            <option value="Acceptable">Acceptable</option>
-                        </select>
-                    </div>
+                        <fieldset>
+                            <label><span>Grading:</span></label>
+                            <select
+                                id="newGrading"
+                                v-model="newFnskuData.grading"
+                                class="form-control"
+                                ref="newGradingInput"
+                            >
+                                <option value="New">New</option>
+                                <option value="Like New">Like New</option>
+                                <option value="Very Good">Very Good</option>
+                                <option value="Good">Good</option>
+                                <option value="Acceptable">Acceptable</option>
+                            </select>
+                        </fieldset>
 
-                    <div class="form-group">
-                        <label for="newStoreName">Store Name:</label>
-                        <select
-                            id="newStoreName"
-                            v-model="newFnskuData.storeName"
-                            class="form-control"
-                            ref="newStoreNameInput"
-                        >
-                            <option value="Allrenewed">Allrenewed</option>
-                            <option value="Renovartech">Renovartech</option>
-                        </select>
-                    </div>
+                        <fieldset>
+                            <label><span>Store Name:</span></label>
+                            <select
+                                id="newStoreName"
+                                v-model="newFnskuData.storeName"
+                                class="form-control"
+                                ref="newStoreNameInput"
+                            >
+                                <option value="Allrenewed">Allrenewed</option>
+                                <option value="Renovartech">Renovartech</option>
+                            </select>
+                        </fieldset>
 
-                    <div class="form-actions">
-                        <button @click="saveNewFnsku" class="btn-save">
+                        <button @click="saveNewFnsku" class="btn btn-process">
                             Save FNSKU
                         </button>
-                        <button
-                            @click="hideInsertFnskuModal"
-                            class="btn-cancel"
-                        >
-                            Cancel
-                        </button>
-                    </div>
+                    </form>
                 </div>
             </div>
         </div>
