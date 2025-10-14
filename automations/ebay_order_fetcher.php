e52fdd9ed92e25d49254676e82d1bd04529565a2
glenpogi <glen@airstaffs.com>
2025-10-15 07:05:23 +0800

    updated fetch ebay order

--- automations/ebay_order_fetcher.php
+++ automations/ebay_order_fetcher.php
@@ -2,8 +2,6 @@
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
-set_time_limit(600);
-ini_set('max_execution_time', 600);
 session_start();
 date_default_timezone_set('America/Los_Angeles');
 
