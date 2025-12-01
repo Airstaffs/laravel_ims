5a1877afe60083dd650c231d84730f3c944a30a0
JDeere <jundell.ggare@gmail.com>
2025-12-02 07:39:03 +0800

    rawr

--- app/Http/Controllers/AttendanceController.php
+++ app/Http/Controllers/AttendanceController.php
@@ -410,7 +410,7 @@ class AttendanceController extends Controller
             ->where('userid', $uid)
             ->value('phone_number');
         */
-        $phone = '9168720618';
+        $phone = '+19163705657';
 
         if ($phone) {
             $smsBody = "Hi {$uname} clocked in at " . $now->format('h:i A') . " (LA). Test Value Rawr";
@@ -418,9 +418,11 @@ class AttendanceController extends Controller
             $smsResult = $this->twilioService->sendSystemSms($phone, $smsBody);
 
             // (Optional) log or attach SMS result
+            /*
             if (!$smsResult['success']) {
-                \Log::warning('Clock-in SMS failed for user ' . $uid . ': ' . $smsResult['error']);
+                \Log::warning('Clock-in SMS failed for user ' . $uid . ': ' . $smsResult[]);
             }
+                */
         }
 
         return response()->json([
