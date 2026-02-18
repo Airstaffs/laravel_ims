f5995b9dbcba19bd1d9e5b952d283eab85829df8
JDeere <jundell.ggare@gmail.com>
2026-02-19 07:05:43 +0800

    rawr

--- vite.config.js
+++ vite.config.js
@@ -3,19 +3,33 @@ import vue from '@vitejs/plugin-vue';
 import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
-    plugins: [
-        laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js', 'resources/js/training.js'],
-            refresh: true,
-        }),
-        vue(),
-    ],
-    resolve: {
-        alias: {
-            vue: 'vue/dist/vue.esm-bundler.js',
-        },
+  plugins: [
+    laravel({
+      input: ['resources/css/app.css', 'resources/js/app.js', 'resources/js/training.js'],
+      refresh: true,
+    }),
+    vue(),
+  ],
+  resolve: {
+    alias: {
+      vue: 'vue/dist/vue.esm-bundler.js',
     },
-    build: {
-        target: 'esnext', // Add this to support top-level await
+  },
+  build: {
+    target: 'esnext',
+  },
+  server: {
+    host: '0.0.0.0',
+    port: 5173,
+    strictPort: true,
+    origin: 'http://localhost:5173',
+    cors: {
+      origin: ['http://localhost:8000'],
+      credentials: true,
     },
+    hmr: {
+      host: 'localhost',
+      port: 5173,
+    },
+  },
 });
