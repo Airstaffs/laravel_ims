82bb703260211d38cc25a058a75735479c13602a
JDeere <jundell.ggare@gmail.com>
2026-03-19 07:06:41 +0800

    rawr

--- routes/web.php
+++ routes/web.php
@@ -1085,7 +1085,7 @@ Route::prefix('api/reconciliation')->middleware(['auth'])->group(function () {
 Route::post('/amazon/search-listings', [ListingController::class, 'searchListings']);
 Route::post('/amazon/listings/update-one', [ListingController::class, 'updateOne']);
 
-Route::post('/amazon/automation/fnsku-search', [PaaAutomationController::class, 'fnskuSearch']);
+Route::post('/amazon/paa/fnsku-search', [PaaAutomationController::class, 'fnskuSearch']);
 Route::post('/amazon/paa/save', [PaaAutomationController::class, 'save']);
 Route::get('/amazon/paa/automations', [PaaAutomationController::class, 'index']);
 Route::get('/amazon/paa/automation/{id}', [PaaAutomationController::class, 'show']);
