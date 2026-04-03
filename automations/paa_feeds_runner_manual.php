eb4a2ded11563cae827dd947b46a97a901de93a1
JDeere <jundell.ggare@gmail.com>
2026-04-04 03:08:17 +0800

    rawr

--- automations/paa_feeds_runner_manual.php
+++ automations/paa_feeds_runner_manual.php
@@ -196,7 +196,7 @@ function db()
         return $mysqli;
     }
 
-    $host = envv('DB_HOST', '127.0.0.1');
+    $host = envv('DB_HOST', 'localhost');
     $port = (int) envv('DB_PORT', 3306);
     $user = envv('DB_USERNAME', '');
     $pass = envv('DB_PASSWORD', '');
